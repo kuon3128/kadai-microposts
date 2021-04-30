6acad6cb8168d145f0e94c07f4409b113fcb365c
kuon <can-dle.01-01on@ezweb.ne.jp>
2021-04-30 19:03:13 +0000

    relationship model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
   has_secure_password
   
   has_many :microposts
+  has_many :relationships
+  has_many :followings, through: :relationships, source: :follow # ＜＝ この行
+  has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
+  has_many :followers, through: :reverses_of_relationship, source: :user # ＜＝ この行
 end

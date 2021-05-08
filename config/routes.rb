8c98e4541cbb968105a74c1f68f47fc9df7dbe00
kuon <can-dle.01-01on@ezweb.ne.jp>
2021-05-08 19:40:12 +0000

    add route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: "toppages#index"
+   root to: "toppages#index"
   
   get "login", to: "sessions#new"
   post "login", to: "sessions#create"
@@ -10,9 +10,11 @@ Rails.application.routes.draw do
     member do
       get :followings
       get :followers
+      get :likes
     end
   end
   
   resources :microposts, only:[:create, :destroy]
   resources :relationships, only: [:create, :destroy]
+  resources :favorites, only: [:create, :destroy]
 end

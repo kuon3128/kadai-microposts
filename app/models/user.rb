ec3161f3c26295bc32bdaf7b947edbbbe78d95cb
kuon <can-dle.01-01on@ezweb.ne.jp>
2021-05-08 19:39:01 +0000

    favorite model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,19 @@
 class User < ApplicationRecord
-  before_save { self.email.downcase!}
+   before_save { self.email.downcase!}
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 255 }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, uniqueness: { case_sensitive: false }
   has_secure_password
   
-  has_many :microposts
+  
   has_many :relationships
   has_many :followings, through: :relationships, source: :follow 
   has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
   has_many :followers, through: :reverses_of_relationship, source: :user 
   
+  has_many :microposts
+  has_many :favorites
+  has_many :likes, through: :favorites, source: :micropost
+
   def follow(other_user)
     unless self == other_user
       self.relationships.find_or_create_by(follow_id: other_user.id)
@@ -27,6 +31,19 @@ class User < ApplicationRecord
   
   def feed_microposts
     Micropost.where(user_id: self.following_ids + [self.id])
+  end
+  
+  def favorite(micropost)
+    self.favorites.find_or_create_by(micropost_id: micropost)
+  end
+
+  def unfavorite(micropost)
+    favorite = self.favorites.find_by(micropost_id: micropost)
+    favorite.destroy if favorite
+  end
+
+  def like?(micropost)
+    self.likes.include?(micropost)
   end
 
 end
